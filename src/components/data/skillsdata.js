dd5a96de8fb02e420927c118a925e49a17ab556d
Vinay <vinay86bhamre@gmail.com>
2022-11-10 02:05:36 +0530

    refactored for mobile responsive view

--- src/components/data/skillsdata.js
+++ src/components/data/skillsdata.js
@@ -4,19 +4,23 @@ export const SkillData = [
     type: "Frontend",
     list: [
       {
-        name: "ReactJs",
-        icon: <i class="devicon-react-original colored"></i>,
+        name: "HTML",
+        icon: <i class="devicon-html5-plain colored"></i>,
+      },
+      {
+        name: "CSS",
+        icon: <i class="devicon-css3-plain colored"></i>,
       },
       {
         name: "JavaScript",
         icon: <i class="devicon-javascript-plain colored"></i>,
       },
       {
-        name: "NextJs",
-        icon: <i class="devicon-nextjs-original colored"></i>,
+        name: "React",
+        icon: <i class="devicon-react-original colored"></i>,
       },
       {
-        name: "TailwindCss",
+        name: "TailwindCSS",
         icon: <i class="devicon-tailwindcss-plain colored"></i>,
       },
     ],
@@ -28,9 +32,22 @@ export const SkillData = [
         name: "Java",
         icon: <i class="devicon-java-plain colored"></i>,
       },
+    ],
+  },
+  {
+    type: "Tools",
+    list: [
       {
-        name: "NodeJs",
-        icon: <i class="devicon-nodejs-plain colored"></i>,
+        name: "Git",
+        icon: <i class="devicon-git-plain colored"></i>,
+      },
+      {
+        name: "VS Code",
+        icon: <i class="devicon-vscode-plain colored"></i>,
+      },
+      {
+        name: "Intellij",
+        icon: <i class="devicon-intellij-plain colored"></i>,
       },
     ],
   },
