c79f77837310049dcc0e4530fa85f788b547ea2b
Vinay <vinay86bhamre@gmail.com>
2022-11-10 00:10:55 +0530

    Updates

---
+++ precache-manifest.dbe1d55c5636660c6ee574a6e8396644.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d93b2fe2cac2cfb3ace0",
+    "url": "/MyPortfolio/static/css/main.efac29d8.chunk.css"
+  },
+  {
+    "revision": "d93b2fe2cac2cfb3ace0",
+    "url": "/MyPortfolio/static/js/main.d93b2fe2.chunk.js"
+  },
+  {
+    "revision": "b2a93e8ecedd7c1fd8ec",
+    "url": "/MyPortfolio/static/js/1.b2a93e8e.chunk.js"
+  },
+  {
+    "revision": "075cb82eb81d0db89bfc",
+    "url": "/MyPortfolio/static/js/2.075cb82e.chunk.js"
+  },
+  {
+    "revision": "14018203dda910b3d1a2",
+    "url": "/MyPortfolio/static/js/runtime~main.14018203.js"
+  },
+  {
+    "revision": "b063e017a3a1a7e34c30a1bf4c709aeb",
+    "url": "/MyPortfolio/static/media/resume.b063e017.pdf"
+  },
+  {
+    "revision": "ada17c578743c123f075eddc7ea0d449",
+    "url": "/MyPortfolio/static/media/linkedin.ada17c57.png"
+  },
+  {
+    "revision": "acf172f26b6761ec94a0d4316bdbf6d4",
+    "url": "/MyPortfolio/static/media/github.acf172f2.png"
+  },
+  {
+    "revision": "36b45b6933bbe63ef8e016a685d3d1de",
+    "url": "/MyPortfolio/static/media/twitter.36b45b69.png"
+  },
+  {
+    "revision": "72cb5467d677275a2b736c7cdde496f6",
+    "url": "/MyPortfolio/static/media/coding.72cb5467.png"
+  },
+  {
+    "revision": "0224f90f6b0fa3870cb3b0b7923ba7ac",
+    "url": "/MyPortfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb35aa27fa39a0840617dca11ea0bea6.js"
+  "/MyPortfolio/precache-manifest.dbe1d55c5636660c6ee574a6e8396644.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/MyPortfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
