125a4271560ad4b7ded47fb810c7e3054d24d55e
Vinay <vinay86bhamre@gmail.com>
2022-11-25 21:21:28 +0530

    shopcart project added

--- src/components/data/project.js
+++ src/components/data/project.js
@@ -1,13 +1,15 @@
+import shopcartImg from "../../assets/shopcart_project.png";
+
 export const ProjectData = [
   {
     id: 1,
-    title: "Random Project 1",
+    title: "Shopcart E-commerce Site",
     about:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse velit consectetur adipisci, quos fugit voluptatum aspernatur consequuntur cumque dolor fugiat.",
-    tags: ["React", "NodeJs", "Firbase"],
-    demo: "www.example.com",
-    github: "www.github.com",
-    image: "https://static.collectui.com/shots/3272787/more-frame-io-large",
+      "An E-commerce site built with React.js, Redux Toolkit and Firebase with features implemented such as Firebase authenthication and managing cart state with redux toolkit.",
+    tags: ["React", "Redux Toolkit", "Firebase"],
+    demo: "https://shopcartz.netlify.app/home",
+    github: "https://github.com/Vinu-devhub/Shopcart",
+    image: shopcartImg,
   },
   {
     id: 2,
