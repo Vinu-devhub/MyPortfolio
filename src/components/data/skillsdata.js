49a18cb143bc2c627111b6a9a43e22c3f94478ea
Vinay <vinay86bhamre@gmail.com>
2023-01-01 20:11:57 +0530

    skills added

--- src/components/data/skillsdata.js
+++ src/components/data/skillsdata.js
@@ -1,57 +1,65 @@
-import React from "react";
+import React from 'react';
 export const SkillData = [
   {
-    type: "Frontend",
+    type: 'Frontend',
     list: [
       {
-        name: "HTML",
-        icon: <i class="devicon-html5-plain colored"></i>,
+        name: 'HTML',
+        icon: <i class='devicon-html5-plain colored'></i>,
       },
       {
-        name: "CSS",
-        icon: <i class="devicon-css3-plain colored"></i>,
+        name: 'CSS',
+        icon: <i class='devicon-css3-plain colored'></i>,
       },
       {
-        name: "JavaScript",
-        icon: <i class="devicon-javascript-plain colored"></i>,
+        name: 'SASS',
+        icon: <i class='devicon-sass-original colored'></i>,
       },
       {
-        name: "React",
-        icon: <i class="devicon-react-original colored"></i>,
+        name: 'JavaScript',
+        icon: <i class='devicon-javascript-plain colored'></i>,
       },
       {
-        name: "Redux",
-        icon: <i class="devicon-redux-original colored"></i>,
+        name: 'React',
+        icon: <i class='devicon-react-original colored'></i>,
       },
       {
-        name: "TailwindCSS",
-        icon: <i class="devicon-tailwindcss-plain colored"></i>,
+        name: 'Redux',
+        icon: <i class='devicon-redux-original colored'></i>,
+      },
+      {
+        name: 'TailwindCSS',
+        icon: <i class='devicon-tailwindcss-plain colored'></i>,
+      },
+      {
+        name: 'TypeScript',
+        icon: <i class='devicon-typescript-plain colored'></i>,
       },
     ],
   },
   {
-    type: "Backend",
+    type: 'Backend',
     list: [
       {
-        name: "Java",
-        icon: <i class="devicon-java-plain colored"></i>,
+        name: 'Java',
+        icon: <i class='devicon-java-plain colored'></i>,
       },
     ],
   },
   {
-    type: "Tools",
+    type: 'Tools',
     list: [
       {
-        name: "Git",
-        icon: <i class="devicon-git-plain colored"></i>,
+        name: 'Git',
+        icon: <i class='devicon-git-plain colored'></i>,
       },
       {
-        name: "VS Code",
-        icon: <i class="devicon-vscode-plain colored"></i>,
+        name: 'VS Code',
+        icon: <i class='devicon-vscode-plain colored'></i>,
       },
       {
-        name: "Intellij",
-        icon: <i class="devicon-intellij-plain colored"></i>,
+        name: 'Intellij',
+        icon: <i class='devicon-intellij-plain colored'></i>,
       },
     ],
   },
